281b5e537ccfb6cd44777bff669ffe088b7846fb
datngoca <datngoc252k3@gmail.com>
2026-03-21 14:54:12 +0700

    feat: Implement a reusable DropDown component and integrate it into the Header for language and profile menus.

---
+++ src/components/Common/DropDown/DropDown.tsx
@@ -0,0 +1,14 @@
+import classNames from "classnames/bind";
+import styles from "./DropDown.module.scss";
+
+const cx = classNames.bind(styles);
+
+const DropDown = () => {
+    return (
+        <div className={cx('wrapper')}>
+            DropDown
+        </div>
+    )
+}
+
+export default DropDown;
--- src/layouts/MainLayout/Header/Header.tsx
+++ src/layouts/MainLayout/Header/Header.tsx
@@ -5,6 +5,8 @@ import { FaSearch, FaBell, FaAngleDown } from "react-icons/fa";
 
 import flagEn from "@/assets/img/flag-en.png";
 import avatar from "@/assets/img/avatar.png";
+import { useState } from "react";
+import DropDown from "@/components/Common/DropDown";
 const cx = classNames.bind(styles);
 
 interface HeaderProps {
@@ -12,6 +14,16 @@ interface HeaderProps {
 }
 
 const Header = ({ className }: HeaderProps) => {
+  const [showLanguageDropDown, setShowLanguageDropDown] = useState(false);
+  const [showProfileDropDown, setShowProfileDropDown] = useState(false);
+
+  const handleLanguageDropDown = () => {
+    setShowLanguageDropDown(!showLanguageDropDown);
+  };
+
+  const handleProfileDropDown = () => {
+    setShowProfileDropDown(!showProfileDropDown);
+  }
   return (
     <header className={cx("header", className)}>
       <div className={cx("header__search")}>
@@ -23,24 +35,39 @@ const Header = ({ className }: HeaderProps) => {
         />
       </div>
       <div className={cx("header__actions")}>
-        <span className={cx("header__notification")}>
+        <div className={cx("header__notification")}>
           <FaBell className={cx("header__notification-icon")} />
           <span className={cx("header__notification-badge")}>3</span>
-        </span>
-        <span className={cx("header__language")}>
+        </div>
+        <div className={cx("header__language")}>
           <img src={flagEn} alt="English" className={cx("header__language-flag")} />
           <span className={cx("header__language-name")}>English</span>
-          <FaAngleDown className={cx("header__dropdown-icon")} />
-        </span>
-        <span className={cx("header__profile")}>
+          <div className={cx("header__dropdown-icon")} onClick={handleLanguageDropDown}>
+            <FaAngleDown />
+          </div>
+          {showLanguageDropDown && (
+            <div className={cx("header__dropdown")}>
+              <DropDown />
+            </div>
+          )}
+        </div>
+        <div className={cx("header__profile")}>
           <img src={avatar} alt="John Doe" className={cx("header__avatar")} />
           <div className={cx("header__infor")}>
             <span className={cx("header__user-name")}>John Doe</span>
             <span className={cx("header__user-role")}>Admin</span>
           </div>
-          <FaAngleDown className={cx("header__dropdown-icon")} />
-        </span>
+          <div className={cx("header__dropdown-icon")} onClick={handleProfileDropDown}>
+            <FaAngleDown />
+          </div>
+          {showProfileDropDown && (
+            <div className={cx("header__dropdown")}>
+              <DropDown />
+            </div>
+          )}
+        </div>
       </div>
+
     </header>
   );
 };
